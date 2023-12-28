6e3f5c54332ec7e5ffab3a2abf264eec249877af
Pavel Lashkov <hutjicsgo@gmail.com>
2023-12-28 23:56:41 +0300

    added expense_list,create,category_list

--- wallet/expenses.py
+++ wallet/expenses.py
@@ -1,17 +1,20 @@
-import requests
-import json
 import telebot
-from telebot import types
 
-from config import TELEGRAM_TOKEN
+from telebot import types
+from datetime import datetime
+
+from openapi_client.models import Expense
+
+from config import TELEGRAM_TOKEN, initialize_expenses_api
 from consts import *
 
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def get_start(message):
     markup = types.ReplyKeyboardMarkup(row_width=2)
-    markup.add(Button.ADD_EXPENSE, Button.LIST_EXPENSE)
+    markup.add(Button.ADD_EXPENSE, Button.LIST_EXPENSE, Button.LIST_CATEGORY)
     bot.send_message(message.chat.id, Message.WELCOME_TO_MONEYBOX, reply_markup=markup)
 
 
@@ -21,13 +24,27 @@ def handle_message(message):
         bot.send_message(message.chat.id, Message.ADD_AMOUNT)
         bot.register_next_step_handler(message, process_amount_step)
     elif message.text == Message.LIST_EXPENSE:
-        bot.send_message(message.chat.id, Message.EXPENSES)
+        expenses_api = initialize_expenses_api()
+        expenses = expenses_api.expense_list_with_http_info()
+        if expenses.status_code == 200:
+             bot.send_message(message.chat.id, f'{Message.EXPENSES}{expenses.raw_data}')
+    elif message.text == Message.LIST_CATEGORY:
+        expenses_api = initialize_expenses_api()
+        response = expenses_api.expensecategory_list_with_http_info()
+        if response.status_code == 200:
+            bot.send_message(message.chat.id, f'{Message.CATEGORIES}{response.raw_data}')
+
 
 def process_amount_step(message):
     try:
-        amount = float(message.text)
-        bot.send_message(message.chat.id, f'Расход в размере {amount} успешно добавлен.')
+        expenses_api = initialize_expenses_api()
+        amount = str(message.text)
+        expense = Expense(id=1, created_at=datetime.now(), updated_at=datetime.now(), amount=amount, category=1, created_by=1, wallet=1, group=1)
+        response = expenses_api.expense_create_with_http_info(expense)
+        if response.status_code == 201:
+            bot.send_message(message.chat.id, Message.ADDED_EXPENSE)
     except ValueError:
-        bot.send_message(message.chat.id, f'Неверная сумма расхода.')
+        bot.send_message(message.chat.id, Message.NOT_EXPENSES)
+
 
 bot.infinity_polling()
